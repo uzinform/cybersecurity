84aa1b46773db647e5b96fe37a1de27fb285d41b
uzinform <sanyokartlife@gmail.com>
2023-10-18 18:24:41 +0300

    ыфв

--- main.py
+++ main.py
@@ -1,21 +1,7 @@
-first_name = input("First name: ")
-middle_name = input("Midle name: ")
-last_name = input("Last name: ")
+num = int(input("Введите возраст: "))
 
-def get_fullname(first_name, middle_name, last_name): 
-   empty = ""
-   if middle_name == empty:
-     return result
-   elif first_name == empty:
-     return data
-   elif last_name == empty or first_name == empty or last_name == empty and first_name == empty:
-     return data
-   else:
-     return fullname
-   
-
-fullname = f"{first_name} {middle_name} {last_name}"
-result = f"{first_name} {last_name}"
-data = f"You miss to input necessary data"
-
-print(get_fullname(first_name, middle_name, last_name))
+try:
+    num = int(input("Введите число: "))
+    print("Все верно. Число:", num)
+except ValueError:
+    print("Это не число.")
