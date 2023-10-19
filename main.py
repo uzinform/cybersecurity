b52b5baf28bdec3c453a411866023ad8b302691a
uzinform <sanyokartlife@gmail.com>
2023-10-19 14:18:42 +0300

    1

--- main.py
+++ main.py
@@ -1,7 +1,25 @@
-num = int(input("Введите возраст: "))
+result = None
+operand = None
+operator = None
+wait_for_number = True
 
-try:
-    num = int(input("Введите число: "))
-    print("Все верно. Число:", num)
-except ValueError:
-    print("Это не число.")
+while True: 
+  operand = input("Введіть число: ")  
+  try:
+     operand = int(operand)  
+  except Exception:
+    print("Введений вами символ - це не число!")   
+  else: 
+   while True: 
+    operator = input("Введіть знак +, -, * або /: ")  
+    if operator == "+" or operator == "-" or operator == "*" or operator == "/":
+      print(operator)
+      break
+    elif operator == "=":
+      break
+    else:
+      print("Ви ввели не вірний знак!")
+  finally:
+    if operand == "=" or operator == "=":      
+      print("Ваш результат: ")
+      break    
